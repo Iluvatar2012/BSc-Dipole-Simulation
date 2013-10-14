9e772eeb69731e5f6227b304a395d22e2e03f6fb
Aiko Bernehed <aiko.bernehed@hhu.de>
2013-10-14 08:41:17 +0200

    minor changes to fileIO.c

--- BSC Dipole Simulation/C-Files/fileIO_v1.c
+++ BSC Dipole Simulation/C-Files/fileIO_v1.c
@@ -130,9 +130,9 @@ struct parameters *read_file(char* file) {
 		return NULL;
 	if ((check = fscanf(infile, "Iterations                           	: %d\n",  (&(param->max_timesteps)))) < 1)
 		return NULL;
-	if ((check = fscanf(infile, "Number of Threads                    : %d\n",  (&(param->thread_number)))) < 1)
+	if ((check = fscanf(infile, "Number of Threads                      : %d\n",  (&(param->thread_number)))) < 1)
 		return NULL;
-	if ((check = fscanf(infile, "Writeouts                            : %d",    (&(param->no_writeouts)))) < 1)
+	if ((check = fscanf(infile, "Writeouts                              : %d",    (&(param->no_writeouts)))) < 1)
 		return NULL;
 
 	// Concatenate current working directory with filename from config file, close the file stream
