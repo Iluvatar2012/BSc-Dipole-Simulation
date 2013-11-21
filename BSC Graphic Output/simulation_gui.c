85b3e3a1cfc86ed376d0ce755775c0579ac4b3e8
Aiko Bernehed <aiko.bernehed@hhu.de>
2013-11-21 19:35:01 +0100

    adjusted the amount of positions read to the correct number, decided to do some formatting and also thought it was a good idea to close everything I decided to open.

--- BSC Graphic Output/simulation_gui.c
+++ BSC Graphic Output/simulation_gui.c
@@ -36,7 +36,7 @@ int hdf5_read (char* file) {
 	herr_t	status;
 
 	// open the file, check whether operation was successful
-	file_id 		= H5Fopen(file, H5F_ACC_RDONLY, H5P_DEFAULT);
+	file_id			= H5Fopen(file, H5F_ACC_RDONLY, H5P_DEFAULT);
 
 	if (file_id < 0) {
 		fprintf(stderr, "File could not be opened, program will now terminate!\nPath: %s\n", file);
@@ -45,17 +45,16 @@ int hdf5_read (char* file) {
 
 	// open the dataset and attributes
 	dataset_id		= H5Dopen2(file_id, "/positions", H5P_DEFAULT);
-
 	attr_N_id		= H5Aopen(dataset_id, "N", H5P_DEFAULT);
 	attr_write_id	= H5Aopen(dataset_id, "Writeouts", H5P_DEFAULT);
 
 	// read the attributes
-	status	= H5Aread(attr_N_id, H5T_NATIVE_INT, &N);
-	status	= H5Aread(attr_write_id, H5T_NATIVE_INT, &steps);
+	status			= H5Aread(attr_N_id, H5T_NATIVE_INT, &N);
+	status			= H5Aread(attr_write_id, H5T_NATIVE_INT, &steps);
 
 	// allocate memory for the temporary data and positions
 	temp			= malloc(2*N*sizeof(double));
-	positions 		= malloc(steps*2*N*sizeof(double));
+	positions 		= malloc((steps+1)*2*N*sizeof(double));
 
 	// offset and dimension of a hyperslab
 	hsize_t	offset[2]	= {0, 0};
@@ -72,8 +71,8 @@ int hdf5_read (char* file) {
 		offset[0] = i;
 
 		// select the hyperslab and read into position array
-		status = H5Sselect_hyperslab(dataspace_id, H5S_SELECT_SET, offset, NULL, slabdim, NULL);
-		status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, tempset_id, dataspace_id, H5P_DEFAULT, temp);
+		status 		= H5Sselect_hyperslab(dataspace_id, H5S_SELECT_SET, offset, NULL, slabdim, NULL);
+		status 		= H5Dread(dataset_id, H5T_NATIVE_DOUBLE, tempset_id, dataspace_id, H5P_DEFAULT, temp);
 
 		// copy the data from the buffer to the position array
 		for (int j=0; j<2*N; j+=2) {
@@ -81,6 +80,14 @@ int hdf5_read (char* file) {
 			positions[2*N*i + j+1]	= temp[j+1];
 		}
 	}
+
+	// close all parts of the simulation
+	status = H5Aclose(attr_N_id);
+	status = H5Aclose(attr_write_id);
+	status = H5Sclose(tempset_id);
+	status = H5Sclose(dataspace_id);
+	status = H5Dclose(dataset_id);
+	status = H5Fclose(file_id);
 
 	// return to caller
 	return EXIT_SUCCESS;
