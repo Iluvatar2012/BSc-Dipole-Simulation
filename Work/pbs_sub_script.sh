3337d32fe7b803ec9aa1db4edf91bbacf8fd7e8f
Aiko Bernehed <aiko.bernehed@hhu.de>
2014-08-11 19:55:33 +0200

    changed pbs subscription script (deprecated)?

--- Work/pbs_sub_script.sh
+++ Work/pbs_sub_script.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
  
+# number of nodes, nodes per cpu and memory of each cpu
 #PBS -l select=1:ncpus=24:mem=1gb
+
+# set execution time
 #PBS -l walltime=24:00:00
-#PBS -r n
-#PBS -N shear_of_binary_colloidal_suspension
+
+# set mail alert at bginning, end and abortion of jobs
+#PBS -m bea
+#PBS -M aiko@thphy.uni-duesseldorf.de
+
+# set Job name and project name
+#PBS -N shearing
 #PBS -A TP2SHEAR
+
+# output of error and stdout files
+#PBS -e ./job.err
+#PBS -o ./job.out
  
 user=`whoami`
  
 #make unique scratch directory on GPFS filesystem
 SCRATCHDIR=/scratch_gs/$USER/$PBS_JOBID
 mkdir -p "$SCRATCHDIR"
-
  
 #some (useful?) output
 LOGFILE=$PBS_O_WORKDIR/$PBS_JOBNAME"."$PBS_JOBID".log"
@@ -28,9 +39,13 @@ echo "Arch       : "$ARCH >> $LOGFILE
 echo "---------------------------" >> $LOGFILE
 echo "RunDir     : "$PBS_O_WORKDIR >> $LOGFILE
 
-#execute gaussian IN the (fast) scratch directory
+#execute program in the (faster) scratch directory
 cd $SCRATCHDIR
-g09 < $PBS_O_WORKDIR/$GaussianInputFilename > $GaussianOutputFilename
+
+# load intel compiler and runtime environment
+module load intel/xe2013
+touch blub
+echo "TODO"
  
 #copy files back from scratch directory
 cp -r $SCRATCHDIR/* $PBS_O_WORKDIR/.
