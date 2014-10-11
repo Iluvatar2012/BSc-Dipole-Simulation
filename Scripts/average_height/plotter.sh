1ac9066e1588f97130550cbf7432edcb6ee94578
Aiko Bernehed <aiko.bernehed@hhu.de>
2014-10-11 22:00:40 +0200

    implemented a good plotting script for the average height

--- Scripts/average_height/plotter.sh
+++ Scripts/average_height/plotter.sh
@@ -11,15 +11,22 @@ set output '$1.png'
 set grid
 
 # set linestyle (a little fat in blue) and fillstyle
-set style line 1 lc 3 lw 3
-set style fill transparent solid 0.5 noborder
+set style line 1 lc 3 lw 4
+set style line 2 lc rgb "forest-green"
+
+set style fill transparent solid 0.4 noborder
 
 # set labels
 
 # set x and yrange
+set xr [0:0.1]
+set yr [0:25]
 
 # set labels and lines if necessary
 
 # plot and return to caller, second argument is the actual data
-plot '$2' u 1:2 w lines lines 1 title ""
+plot '$2' u 1:2:(\$2-\$3) w filledcurves lines 2 title "", \
+	 '$2' u 1:2:(\$2+\$3) w filledcurves lines 2 title "", \
+	 '$2' u 1:2 w lines lines 1 title ""
+
 EOF
