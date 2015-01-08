b2c49c73294317d3bbe0532fc987df57e29bf3bb
Aiko Bernehed <aiko.bernehed@hhu.de>
2015-01-08 20:41:49 +0100

    added the movement script and small script for getting final difference height

--- Scripts/final_height/final_diff.sh
+++ Scripts/final_height/final_diff.sh
@@ -18,6 +18,14 @@ do
 	M="echo $F | cut -d '_' -f 2"
 	G="echo $F | cut -d '_' -f 4"
 
-	
+	# read the file and write to plaintext, execute ruby script
+	./write_to_file $F temp 0.001 100000
+	./final_step.rb temp $M $G
 
+	# plot the data 
+	# TODO, write a plotter
+
+	# remove temporary files
+	rm temp
+	# rm final_stat
 done
